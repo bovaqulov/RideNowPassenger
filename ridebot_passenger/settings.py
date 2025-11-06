0d24f889f377a473557260621fee3890c741c419
bovaqulov <bovaqulov@gmail.com>
2025-11-06 13:48:45 +0500

    add redis

--- ridebot_passenger/settings.py
+++ ridebot_passenger/settings.py
@@ -4,10 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-)q--it3x@0!e7s!#_w)2s*^165e!c04(4!fvn9*wcaq-)j&m1g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
